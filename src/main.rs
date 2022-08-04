c62ee6a04445b5fa2f0ccebabb80b27d362a4fef
Emil Koutanov <ekoutanov@gmail.com>
2022-08-04 16:46:55 +1000

    Command-line argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,134 @@
-use std::{error, fs, io, process};
+use clap::Parser;
+use home;
+use serbzip::codec::dict::Dict;
+use serbzip::codec::{compress_line, expand_line};
+use std::ffi::OsStr;
 use std::fs::File;
 use std::io::{BufReader, BufWriter, Error, Read, Write};
-use std::path::Path;
-use serbzip::codec::{compress_line, expand_line};
-use serbzip::codec::dict::Dict;
+use std::path::{Path, PathBuf};
+use std::{error, fs, io, process};
+
+/// A quasi-lossless Balkanoidal meta-lingual compressor
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Compress
+    #[clap(short, long, value_parser)]
+    compress: bool,
+
+    /// Expand
+    #[clap(short = 'x', long, value_parser)]
+    expand: bool,
+
+    /// Dictionary file (.txt for a text file, .img for binary image)
+    #[clap(short, long, value_parser)]
+    dictionary: Option<String>,
+
+    /// Input file (or '-' for stdin)
+    #[clap(short, long, value_parser)]
+    input_file: Option<String>,
+
+    /// Output file (or '-' for stdout)
+    #[clap(short, long, value_parser)]
+    output_file: Option<String>,
+
+    /// Output file for compiling the dictionary image
+    #[clap(short = 'm', long, value_parser)]
+    dictionary_image_output_file: Option<String>,
+}
 
 fn main() -> Result<(), Box<dyn error::Error>> {
-    let mut reader = BufReader::new(File::open("dict.txt")?);
-    let dict = Dict::read_from_text_file(&mut reader)?;
-    let mut writer = BufWriter::new(File::create("dict.img")?);
-    dict.write_to_binary_image(&mut writer)?;
-    writer.flush().unwrap();
-    drop(writer);
+    let mut args = Args::parse();
+    //eprintln!("args: {args:?}");
 
-    // let mut reader = BufReader::new(File::open("dict.img")?);
-    // let dict = Dict::load_from_binary_image(&mut reader)?;
-    // println!("Loaded dict with {} words", dict.count());
-    // compress_from_stdin(&dict);
-    expand_from_stdin(&dict);
+    let dict_file: Option<String> = args.dictionary.take();
+    let dict_path = dict_file
+        .map(|path| {
+            let specified_path = Path::new(&path);
+            if specified_path.exists() {
+                specified_path.to_owned()
+            } else {
+                eprintln!("Failed to open dictionary file {specified_path:?}");
+                process::exit(1);
+            }
+        })
+        .or_else(|| {
+            let local_img_path = Path::new("dict.img");
+            if local_img_path.exists() {
+                Some(local_img_path.to_owned())
+            } else {
+                let local_txt_path = Path::new("dict.txt");
+                if local_txt_path.exists() {
+                    Some(local_txt_path.to_owned())
+                } else {
+                    // resolved to ${HOME} (in *nix-based systems) or %USERPROFILE% in Windows
+                    match home::home_dir() {
+                        None => {
+                            eprintln!("The user's home directory could not be located; please specify the dictionary file");
+                            process::exit(1);
+                        }
+                        Some(path) => {
+                            let home_img_path = path.as_path().join(Path::new("/dict.img"));
+                            if home_img_path.exists() {
+                                Some(home_img_path.to_owned())
+                            } else {
+                                eprintln!("The user's home directory could not be located; please specify the dictionary file");
+                                process::exit(1);
+                            }
+                        }
+                    }
+                }
+            }
+        }).unwrap();
+
+    // read the dictionary from either the user-supplied or default path
+    let dict = match dict_path.extension() {
+        None => {
+            eprintln!("Unsupported dictionary format for {dict_path:?}: only .txt and .img files can be read");
+            process::exit(1);
+        }
+        Some(extension) => {
+            if extension == "txt" {
+                let mut reader = BufReader::new(File::open(dict_path)?);
+                Dict::read_from_text_file(&mut reader)?
+            } else if extension == "img" {
+                let mut reader = BufReader::new(File::open(dict_path)?);
+                Dict::read_from_binary_image(&mut reader)?
+            } else {
+                eprintln!("Unsupported dictionary format for {dict_path:?}: only .txt and .img files can be read");
+                process::exit(1);
+            }
+        }
+    };
+
+    // if the -m option has been set, serialize dict to a user-specified file
+    if let Some(image_output_file) = &args.dictionary_image_output_file {
+        if !image_output_file.to_string().ends_with(".img") {
+            eprintln!("Only .img files are supported for compiled dictionaries");
+            process::exit(1);
+        }
+        eprintln!("Writing compiled dictionary image to {image_output_file}");
+        let mut writer = BufWriter::new(File::create(image_output_file)?);
+        dict.write_to_binary_image(&mut writer)?;
+        writer.flush().unwrap();
+        drop(writer);
+        process::exit(0);
+    }
+
+    if !(args.compress ^ args.expand) {
+        eprintln!("Either one of --compress or --expand must be specified");
+        process::exit(1)
+    }
+
+    if args.compress {
+        compress_from_stdin(&dict);
+    } else {
+        expand_from_stdin(&dict);
+    }
     Ok(())
 }
 
 fn compress_from_stdin(dict: &Dict) {
-    // let mut read_buf = String::new();
-    // loop {
-    //     match io::stdin().read_line(&mut read_buf) {
-    //         Ok(0) => {
-    //             process::exit(0)
-    //         }
-    //         Ok(size) => {
-    //             let compressed = compress_line(&dict, &read_buf[0..size-1]);
-    //             println!("{}", compressed);
-    //             read_buf.clear();
-    //         }
-    //         Err(_) => {
-    //             process::exit(1)
-    //         }
-    //     }
-    // }
     process_from_stdin(|line| compress_line(&dict, line));
 }
 
@@ -49,17 +140,13 @@ fn process_from_stdin(mut processor: impl FnMut(&str) -> String) {
     let mut read_buf = String::new();
     loop {
         match io::stdin().read_line(&mut read_buf) {
-            Ok(0) => {
-                process::exit(0)
-            }
+            Ok(0) => process::exit(0),
             Ok(size) => {
-                let output = processor(&read_buf[0..size-1]);
+                let output = processor(&read_buf[0..size - 1]);
                 println!("{}", output);
                 read_buf.clear();
             }
-            Err(_) => {
-                process::exit(1)
-            }
+            Err(_) => process::exit(1),
         }
     }
 }
